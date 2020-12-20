d16b9e5f49502da4e435137d1aa95eb4e9c0a3dc
Fedor <approachingminimums@pm.me>
2020-12-20 19:51:07 +0300

    added koridor 307 render

---
+++ 307/data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--307--archicad-23--corona-render-5",
+      "name": "Коридор 307 кафедры, Archicad 23, визуализация Corona Render 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.040118992657610875,
+        "pitch": 0.06737876583635938,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Коридор 307 кафедры",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
